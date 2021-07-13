df6232a90e2131021ee3200831d34b1620a0443e
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2021-07-13 17:21:52 +0700

    add pubsub function and workflow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,31 @@
 import * as functions from "firebase-functions";
+import firestore = require("@google-cloud/firestore");
+const client = new firestore.v1.FirestoreAdminClient();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Replace BUCKET_NAME
+const bucket = "gs://gep-firestore-backup";
+
+exports.scheduledFirestoreExport = functions.pubsub
+    .schedule("every 2 minutes")
+    .onRun((context) => {
+      const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+      const databaseName =
+    client.databasePath(projectId!, "(default)");
+
+      return client.exportDocuments({
+        name: databaseName,
+        outputUriPrefix: bucket,
+        // Leave collectionIds empty to export all collections
+        // or set to a list of collection IDs to export,
+        // collectionIds: ["users", "posts"]
+        collectionIds: [],
+      })
+          .then((responses) => {
+            const response = responses[0];
+            console.log(`Operation Name: ${response["name"]}`);
+          })
+          .catch((err) => {
+            console.error(err);
+            throw new Error("Export operation failed");
+          });
+    });
